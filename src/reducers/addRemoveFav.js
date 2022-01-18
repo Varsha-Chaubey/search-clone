5805810e667cfd0f337caf4c460d044c6a2d9569
Varsha Chaubey <93591654+Varsha-Chaubey@users.noreply.github.com>
2022-01-18 16:03:41 +0530

    Update addRemoveFav.js

--- src/reducers/addRemoveFav.js
+++ src/reducers/addRemoveFav.js
@@ -1,15 +1,14 @@
-const INIT_STATE = "No Favorite selected";
+const INIT_STATE = [];
 
 
 const favoriteUnFavorite = (state = INIT_STATE, action) => {
     
     // console.log(action.title)
     switch(action.type){
-        case "FAVORITE": return action.title;
-        case "UNFAVORITE": return state-action.title;
+        case "FAVORITE": return [...state, action.title];
+        case "UNFAVORITE": return state.filter(s => s !== action.title);
         default: return state;
     }
-    
 }
 
 export default favoriteUnFavorite; 
